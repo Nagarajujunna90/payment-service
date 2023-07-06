5a03ac3c9a66f91b07e61d5d24ae20704a121cb1
njunna <nagarajujunna90@gmail.com>
2023-07-06 21:29:27 +0530

    updated files

--- src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -1,8 +1,10 @@
 package com.example.paymentservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(info = @Info(title = "payment-service"))
 @SpringBootApplication
 public class PaymentServiceApplication {
 
